a954985b40240ce1519495b416959b216edd9cdd
iLYuSha <ilyusha71@gmail.com>
2019-03-29 20:08:59 +0800

    Initial commit

---
+++ Assets/DoraraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoraraController : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        anim.SetFloat("V", Input.GetAxis("Vertical"));
+        anim.SetFloat("H", Input.GetAxis("Horizontal"));
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Wakaka-Character
